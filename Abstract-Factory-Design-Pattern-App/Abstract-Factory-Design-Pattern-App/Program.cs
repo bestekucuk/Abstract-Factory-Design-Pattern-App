5e801778420d592a650dfbbb5e16956d72086335
beste küçük <peribeste48@gmail.com>
2022-05-24 11:17:55 +0300

    intial commit
    intial commit

---
+++ Abstract-Factory-Design-Pattern-App/Abstract-Factory-Design-Pattern-App/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Abstract_Factory_Design_Pattern_App
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Uygulamanın ana girdi noktası.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
